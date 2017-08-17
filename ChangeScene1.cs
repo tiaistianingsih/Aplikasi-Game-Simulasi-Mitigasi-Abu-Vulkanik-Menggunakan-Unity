1b22e0f1e066be27bfe67e353cdad525d8ce6d1e
tiaistianingsih <31027472+tiaistianingsih@users.noreply.github.com>
2017-08-17 20:19:15 +0700

    1. File Baru Ditambah

--- ChangeScene1.cs
+++ ChangeScene1.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+//Tia Istianingsih Sistem Informasi Universitas Gunadarma
+//email: tiaistianingsih@student.gunadarma.ac.id
+//http://1A114743.student.gunadarma.ac.id
+//http://www.gunadarma.ac.id
+//Dosen Pembimbing : Dr.M Akbar Marwan, ST., MMSI
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
