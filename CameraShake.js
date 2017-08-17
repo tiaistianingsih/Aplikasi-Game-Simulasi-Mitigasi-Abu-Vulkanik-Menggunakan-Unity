486f7e2abc6342a1a2e633e44b42b23115bcf932
tiaistianingsih <31027472+tiaistianingsih@users.noreply.github.com>
2017-08-17 20:15:50 +0700

    1. File Baru Ditambah

--- CameraShake.js
+++ CameraShake.js
@@ -1,4 +1,10 @@
-﻿#pragma strict
+//Tia Istianingsih Sistem Informasi Universitas Gunadarma
+//email: tiaistianingsih@student.gunadarma.ac.id
+//http://1A114743.student.gunadarma.ac.id
+//http://www.gunadarma.ac.id
+//Dosen Pembimbing : Dr.M Akbar Marwan, ST., MMSI
+
+#pragma strict
 
 var startingShakeDistance : float = 0.8;
 var decreasePercentage : float = 0.5;
